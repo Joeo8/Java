ba86e9d8830561de3376d7d1791e9bafc67b1ab8
Joeo8 <joe_ob@qq.com>
2021-02-15 21:17:17 +0800

    File

---
+++ Java_Code/src/Joeo8/File05.java
@@ -0,0 +1,38 @@
+package Joeo8;
+
+import java.io.File;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Joeo8
+ * Time: 21:07
+ * Description: No Description
+ */
+public class File05 {
+    public static void main(String[] args) {
+        //File遍历功能方法
+//        showList();
+        showListFiles();
+    }
+
+    public static void showList() {
+        //以String类型接收获取到的目录或子文件
+        File file = new File("C:\\Java");
+        String[] list = file.list();
+        for (String str :
+                list) {
+            System.out.println(str);
+        }
+    }
+
+    public static void showListFiles() {
+        //以File类型接收目录和子文件
+        File file = new File("C:\\Java");
+        File[] fileList = file.listFiles();
+        assert fileList != null;
+        for (File f :
+                fileList) {
+            System.out.println(f);
+        }
+    }
+}
