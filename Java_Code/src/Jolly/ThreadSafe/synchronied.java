83aa033d66cb3f1d8be1361573ab955a9b6359ae
Joeo8 <joe_ob@qq.com>
2021-02-17 16:25:47 +0800

    Synchronized

--- Java_Code/src/Jolly/ThreadSafe/synchronied.java
+++ Java_Code/src/Jolly/ThreadSafe/synchronied.java
@@ -11,7 +11,8 @@ public class synchronied {
     public static void main(String[] args) {
         //synchronized 同步代码块
         //原理: 让多个线程使用同一个锁对象(把同步代码块锁住,保证只有一个线程在同步代码块中执行)
-
+        //总结: 同步中的线程没有执行完毕不会释放锁;同步外的线程没有锁,进不去
+        //缺点: 程序频繁的判断锁,获取锁,释放锁,程序的效率降低
         synTickets run = new synTickets();
 
         Thread th01 = new Thread(run);
